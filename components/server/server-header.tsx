f27284aeae8ce55ef399e5d12c04a0b19d93ce76
Jesse Cristobal <jcris020@fiu.edu>
2024-02-26 10:21:19 -0500

    Implemented invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,9 +2,10 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
-import { ChevronDown, Logout, PlusCircle, Settings, Trash, Users, UserPlus } from "lucide-react";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, Users, UserPlus } from "lucide-react";
 
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { useModal } from "../../hooks/use-modal-store";
 
 
 interface ServerHeaderProps {
@@ -13,6 +14,8 @@ interface ServerHeaderProps {
 };
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+    const { onOpen } = useModal();
+
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -36,7 +39,11 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
             >
                 {isModerator && (
-                    <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={() => onOpen("invite", { server })}
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 
+                            text-sm cursor-pointer"
+                        >
                         Invite People
                         <UserPlus className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
@@ -71,7 +78,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 {!isAdmin && (
                     <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
-                        <Logout className="h-4 w-4 ml-auto" />
+                        <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
             </DropdownMenuContent>
